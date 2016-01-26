edbac6d9d1079f73a99b1168beb999de5348bf60
Agustin Silva <silva.agustinn@gmail.com>
2016-01-26 19:05:41 -0300

    Agrega validations al modelo Producto

--- app/models/producto.rb
+++ app/models/producto.rb
@@ -1,6 +1,10 @@
 class Producto < ActiveRecord::Base
   before_save :default_stock
 
+  validates :nombre, presence: true
+  validates :precio, numericality: {greater_than_or_equal_to: 0.01}
+  validates :stock, numericality: {greather_than_or_equal_to: 0}
+
   private
 
   def default_stock
